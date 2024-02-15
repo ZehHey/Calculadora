65c5d99588eaa3f987bbe3279e9c92f63b26fb9f
José C B Junior <142643035+ZehHey@users.noreply.github.com>
2024-02-15 00:45:05 -0300

    calculadora finalizada

---
+++ script.js
@@ -0,0 +1,72 @@
+var lista = []
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    const isEnter = (key === 'Enter' || event.code === 'Enter');
+    const isBackspace = (key === 'Backspace' || event.code === 'Backspace');
+    const isEsc = (key === 'Escape' || event.code === 'Escape')
+    if (/[0-9+\-*/.=]/.test(key) || isEnter || isBackspace || isEsc) {
+        event.preventDefault();
+        if (key === 'Enter') {
+            igual();
+        } else if (key === 'Backspace') {
+            event.preventDefault();
+            apagar();
+        }else if(key === 'Escape'){
+            event.preventDefault();
+            ac();
+        }else{
+            add(key);
+        }
+    }
+});
+
+function add(num){
+    var numero = document.getElementById('res').innerHTML;
+    document.getElementById('res').innerHTML = numero + num
+}
+
+function ac(){
+    document.getElementById('res').innerHTML = ''
+}
+
+function apagar() {
+    var tela = document.getElementById('res');
+    var conteudoAtual = tela.innerHTML;
+    tela.innerHTML = conteudoAtual.substring(0, conteudoAtual.length - 1);
+}
+
+var adicionaAbertura = true;
+function parenteses() {
+    var tela = document.getElementById('res');
+    
+    if (adicionaAbertura) {
+        tela.innerHTML += '(';
+    } else {
+        tela.innerHTML += ')';
+    }
+    
+    adicionaAbertura = !adicionaAbertura;
+}
+function porcento(){
+    var tela = document.getElementById('res');
+    var conteudoAtual = tela.innerHTML
+    conteudoAtual = conteudoAtual.replace('%','')
+    var resultado = eval(conteudoAtual)/100
+    tela.innerHTML = resultado
+
+}
+function igual(){
+    var tela = document.getElementById('res')
+    if(tela){
+        var expressão = tela.innerHTML
+        
+        var resultado = eval(expressão)
+        
+        resultado = parseFloat(resultado.toFixed(2))
+        tela.innerHTML = resultado
+        
+    } else{
+        document.getElementById('res').innerHTML = ''
+    }
+}
